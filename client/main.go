766275eda31eb11071f871dced07a911b682f4d6
basefly <basefly@outlook.com>
2021-03-16 22:46:46 +0800

    dockerfile && client支持 仅 pull 不 push

--- client/main.go
+++ client/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/atotto/clipboard"
 	"github.com/hhstu/clipboard-sync/pkg"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"reflect"
 	"time"
@@ -15,9 +16,14 @@ import (
 
 var data pkg.Object
 var server string
+var needPush bool
+var token string
 
 func init() {
 	flag.StringVar(&server, "server", "http://localhost:8080", "server url")
+	flag.BoolVar(&needPush, "needPush", true, "push or not")
+	flag.StringVar(&token, "token", "lc666", "token")
+	flag.Parse()
 }
 func main() {
 	ticker := time.NewTicker(1 * time.Second)
@@ -34,30 +40,37 @@ func pull() {
 	ret, err := http.Get(pull)
 
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
+	ret.Header.Add("token", token)
 	defer ret.Body.Close()
 
 	body, err := ioutil.ReadAll(ret.Body)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
 	var tmp pkg.Object
 	err = json.Unmarshal(body, &tmp)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
 	if !reflect.DeepEqual(data, tmp) {
 		data = tmp
 		clipboard.WriteAll(data.Data)
 	} else {
-		tmp, err := clipboard.ReadAll()
-		if err != nil {
-			panic(err)
-		}
-		if tmp != data.Data {
-			data.Data = tmp
-			push()
+		if needPush {
+			tmp, err := clipboard.ReadAll()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if tmp != data.Data {
+				data.Data = tmp
+				push()
+			}
 		}
 	}
 
@@ -67,14 +80,17 @@ func push() {
 	push := fmt.Sprintf("%s/push", server)
 	dataStr, err := json.Marshal(data)
 	req, err := http.NewRequest("POST", push, bytes.NewBuffer(dataStr))
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	// req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Content-Type", "application/json")
-
+	req.Header.Set("token", token)
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	_, err = client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
-	defer resp.Body.Close()
 
 }
--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/hhstu/clipboard-sync/pkg"
 	"io/ioutil"
@@ -10,31 +11,43 @@ import (
 )
 
 var data pkg.Object
+var token string
 
 func push(w http.ResponseWriter, r *http.Request) {
 
+	if r.Header.Get("token") != token {
+		return
+	}
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
 
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		panic(err)
+		log.Println(err, body)
+		return
 	}
 	log.Println("get msg from ", r.RemoteAddr, "data: ", data.Data)
 	fmt.Fprintf(w, "ok")
 }
 
 func pull(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("token") != token {
+		return
+	}
 	resp, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		log.Println(err, resp)
 	}
 
 	fmt.Fprintf(w, string(resp))
 }
-
+func init() {
+	flag.StringVar(&token, "token", "lc666", "token")
+	flag.Parse()
+}
 func main() {
 	http.HandleFunc("/push", push)
 	http.HandleFunc("/pull", pull)
